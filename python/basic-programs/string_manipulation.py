42b2033b7be2d558ab36c61ac41875052adfa088
William Meredith <wm00026@mix.wvu.edu>
2025-11-08 16:01:16 -0500

    Basic updates

---
+++ python/basic-programs/string_manipulation.py
@@ -0,0 +1,12 @@
+"""
+This is a basic program that deals with the basic manipulation of Strings.
+
+Author: @wm00026
+"""
+
+def string_amipulatino(astring):
+    astring
+
+mystring = input("Type something: ")
+
+string_manipulation(mystring)
